79bc81cff40af25c12f2512f2258d279f7143c85
asgervelling <asger.skov.velling@gmail.com>
2025-06-05 11:35:12 +0200

    Started using Excel to check correctness of SQL queries

---
+++ src/app/api/total-revenue/route.ts
@@ -0,0 +1,11 @@
+/**
+ * Total revenue = sum (rate * quantity + tax - discount).
+ * 
+ * The rate is the price of an item, excluding tax.
+ * Similarly, tax is not a part of profit calculation, rate is.
+ */
+export async function GET() {
+  try {
+    const 
+  }
+}
