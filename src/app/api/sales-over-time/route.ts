2970c4405ee5b96303c5649e0e417fcdde95b906
asgervelling <asger.skov.velling@gmail.com>
2025-06-05 14:31:45 +0200

    Made sales over time api route (line chart)

---
+++ src/app/api/sales-over-time/route.ts
@@ -0,0 +1,29 @@
+import { pool } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+/**
+ * Sales over Time.
+ * Date i = sum (rate * quantity + tax - discount).
+ */
+export async function GET() {
+  try {
+    const { rows } = await pool.query(
+      `SELECT DATE(t.date) AS day,
+      SUM(i.rate * i.quantity + i.tax - i.discount) AS revenue
+      FROM transaction_items i
+      JOIN transactions t ON i.tid = t.tid
+      GROUP BY day
+      ORDER BY day`
+    );
+    const salesOverTime = rows;
+    if (salesOverTime.length === 0) {
+      return NextResponse.json(
+        { error: "0 results found" },
+        { status: 404 }
+      )
+    }
+    return NextResponse.json({ salesOverTime })
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
